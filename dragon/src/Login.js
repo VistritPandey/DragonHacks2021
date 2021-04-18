97db3281160deefe1573da9f2322d50e6a6c22ca
vistritpandey <vistritpandey@gmail.com>
2021-04-18 11:28:55 +0530

    Bug fixed

---
+++ dragon/src/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Login.css";
+import { provider, auth } from "./Firebase";
+import { Button } from "@material-ui/core";
+import logo from "./Logo.jpg";
+
+function Login() {
+  const signIn = (e) => {
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
+
+  return (
+    <div className="login">
+      <img src={logo} alt="" />
+      <Button onClick={signIn}>Login</Button>
+    </div>
+  );
+}
+
+export default Login;
